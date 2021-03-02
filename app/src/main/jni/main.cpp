2cc7e722aae5928875598ea505e3a23fc0202472
username <email@email.com>
2021-03-03 00:20:55 +0300

    ***

---
+++ app/src/main/jni/main.cpp
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <android/log.h>
+#include <dirent.h>
+#include <sys/system_properties.h>
+
+using namespace std;
+                   
+int main(int argc, char **argv)
+{
+    return (EXIT_SUCCESS);
+}
